e6ff61df712174e20fbc5093cf03467bd50d8a85
nzafranf <nzfadhilll@gmail.com>
2026-03-05 13:21:33 +0700

    [GREEN] DummyControllerTest.java passed

--- src/test/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/controller/DummyControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/controller/DummyControllerTest.java
@@ -1,44 +1,20 @@
-package id.ac.ui.cs.advprog.jsonbackend.controller;
+package id.ac.ui.cs.advprog.jsonbackend.authprofile.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@WebMvcTest(DummyController.class)
 class DummyControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
     private DummyController dummyController;
 
-    @Test
-    void testSayHelloReturnsOk() throws Exception {
-        mockMvc.perform(get("/api/hello"))
-                .andExpect(status().isOk());
+    @BeforeEach
+    void setUp() {
+        dummyController = new DummyController();
     }
 
     @Test
-    void testSayHelloReturnsCorrectBody() throws Exception {
-        mockMvc.perform(get("/api/hello"))
-                .andExpect(content().string("Hello from Backend!"));
-    }
-
-    @Test
-    void testSayHelloReturnsCorrectContentType() throws Exception {
-        mockMvc.perform(get("/api/hello"))
-                .andExpect(content().contentType("text/plain;charset=UTF-8"));
-    }
-
-    @Test
-    void testSayHelloDirectCall() {
+    void testSayHelloReturnsCorrectBody() {
         assertEquals("Hello from Backend!", dummyController.sayHello());
     }
 
@@ -46,5 +22,4 @@ class DummyControllerTest {
     void testDummyControllerNotNull() {
         assertNotNull(dummyController);
     }
-
 }
