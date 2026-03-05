906df740e50d14b24115994a69f73c2022146ab3
nzafranf <nzfadhilll@gmail.com>
2026-03-05 18:09:26 +0700

    [REFACTOR] LoginServiceImpl.java use passwordEncoder.matches for secure authentication

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/service/LoginServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/service/LoginServiceImpl.java
@@ -4,36 +4,32 @@ import id.ac.ui.cs.advprog.jsonbackend.authprofile.dto.UserLoginRequest;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.exception.UserNotFoundException;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.exception.WrongPasswordException;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.User;
-import id.ac.ui.cs.advprog.jsonbackend.authprofile.exception.*;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class LoginServiceImpl implements LoginService {
-    private final UserRepository userRepository;
 
-    @Autowired
-    public LoginServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User login(UserLoginRequest dto) {
-        User user;
         String dtoEmail = dto.getEmail();
         Optional<User> optionalUser = userRepository.findByEmail(dtoEmail);
 
         if (optionalUser.isEmpty()) {
             throw new UserNotFoundException("User not found with email: " + dtoEmail);
         }
-        else {
-            user = optionalUser.get();
-        }
 
-        if (!dto.getPassword().matches(user.getPassword())) {
+        User user = optionalUser.get();
+
+        if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
             throw new WrongPasswordException("Wrong password");
         }
 
