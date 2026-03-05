aeb47f9a0b76f6c56e03371f2b7d717bebb2863e
nzafranf <nzfadhilll@gmail.com>
2026-03-05 18:09:56 +0700

    [REFACTOR] UserLoginResponse.java implementation of DTO pattern to protect sensitive data

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/dto/UserLoginResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/dto/UserLoginResponse.java
@@ -1,13 +1,24 @@
 package id.ac.ui.cs.advprog.jsonbackend.authprofile.dto;
 
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.User;
+import lombok.Builder;
+import java.util.UUID;
 
+@Builder
 public record UserLoginResponse (
-    User user
+    UUID id,
+    String username,
+    String email,
+    String role,
+    String status
 ) {
-
-    public UserLoginResponse(User user) {
-        this.user = user;
-        this.user.setPassword("");
+    public static UserLoginResponse fromUser(User user) {
+        return UserLoginResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .role(user.getRole().name())
+                .status(user.getStatus().name())
+                .build();
     }
 }
