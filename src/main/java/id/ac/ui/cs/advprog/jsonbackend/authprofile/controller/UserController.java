fd863e3d4d1d88726e423c201914f72116b4a402
nzafranf <nzfadhilll@gmail.com>
2026-03-05 18:09:57 +0700

    [REFACTOR] UserController.java return DTOs instead of entities for security

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/controller/UserController.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/controller/UserController.java
@@ -1,30 +1,29 @@
 package id.ac.ui.cs.advprog.jsonbackend.authprofile.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import id.ac.ui.cs.advprog.jsonbackend.authprofile.dto.UserLoginResponse;
+import id.ac.ui.cs.advprog.jsonbackend.authprofile.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import id.ac.ui.cs.advprog.jsonbackend.authprofile.service.UserService;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
+    private final UserService userService;
 
     @GetMapping("/getUsers")
-    public ResponseEntity<?> getUsers() {
-
-        return new ResponseEntity<>(this.userService.getAllUsers(), HttpStatus.OK);
+    public ResponseEntity<List<UserLoginResponse>> getUsers() {
+        List<UserLoginResponse> users = userService.getAllUsers().stream()
+                .map(UserLoginResponse::fromUser)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
-
 }
