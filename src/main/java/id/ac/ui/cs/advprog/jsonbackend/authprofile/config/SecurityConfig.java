eb0b2f7dc7fd16ba5dc4f32a9e26432acc97e780
nzafranf <nzfadhilll@gmail.com>
2026-03-05 18:09:25 +0700

    [REFACTOR] SecurityConfig.java add BCryptPasswordEncoder for secure password hashing

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/config/SecurityConfig.java
@@ -9,9 +9,17 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
