02088b38945be663596ffd4bf4f43db7e5d3cdb2
nzafranf <nzfadhilll@gmail.com>
2026-03-05 18:09:42 +0700

    [REFACTOR] UpgradeRequestRetrievalServiceImpl.java use RequiredArgsConstructor for clean dependency injection

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/service/UpgradeRequestRetrievalServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/service/UpgradeRequestRetrievalServiceImpl.java
@@ -3,20 +3,17 @@ package id.ac.ui.cs.advprog.jsonbackend.authprofile.service;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.UpgradeRequest;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.User;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.repository.UpgradeRequestRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UpgradeRequestRetrievalServiceImpl implements UpgradeRequestRetrievalService {
-    private final UpgradeRequestRepository upgradeRequestRepository;
 
-    @Autowired
-    public UpgradeRequestRetrievalServiceImpl(UpgradeRequestRepository upgradeRequestRepository) {
-        this.upgradeRequestRepository = upgradeRequestRepository;
-    }
+    private final UpgradeRequestRepository upgradeRequestRepository;
 
     @Override
     public List<UpgradeRequest> getAllRequests() {
@@ -27,5 +24,4 @@ public class UpgradeRequestRetrievalServiceImpl implements UpgradeRequestRetriev
     public Optional<UpgradeRequest> getRequestByUsername(User user) {
         return upgradeRequestRepository.findByRequesterUser(user);
     }
-
 }
