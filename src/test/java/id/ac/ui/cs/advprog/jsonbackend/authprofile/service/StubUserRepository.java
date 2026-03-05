f62be110226578ccef630bb9e75c791451bbf98c
nzafranf <nzfadhilll@gmail.com>
2026-03-05 13:21:44 +0700

    [GREEN] StubUserRepository.java passed

--- src/test/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/service/StubUserRepository.java
+++ src/test/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/service/StubUserRepository.java
@@ -1,7 +1,7 @@
-package id.ac.ui.cs.advprog.jsonbackend.service;
+package id.ac.ui.cs.advprog.jsonbackend.authprofile.service;
 
-import id.ac.ui.cs.advprog.jsonbackend.model.User;
-import id.ac.ui.cs.advprog.jsonbackend.repository.UserRepository;
+import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.User;
+import id.ac.ui.cs.advprog.jsonbackend.authprofile.repository.UserRepository;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
