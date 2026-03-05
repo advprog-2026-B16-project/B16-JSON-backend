585041de38ab11c06ed744cd63f25f0edfc42e13
nzafranf <nzfadhilll@gmail.com>
2026-03-05 18:09:42 +0700

    [REFACTOR] UpgradeRequestRepository.java update to use UUID primary key

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/repository/UpgradeRequestRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/repository/UpgradeRequestRepository.java
@@ -1,16 +1,14 @@
 package id.ac.ui.cs.advprog.jsonbackend.authprofile.repository;
 
-import java.util.Optional;
-
-import java.util.UUID;
-
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.UpgradeRequest;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface UpgradeRequestRepository extends JpaRepository<UpgradeRequest, UUID> {
-    Optional<UpgradeRequest> findByRequesterUser(User user);
-//    Optional<User> findByEmail(String email);
+    Optional<UpgradeRequest> findByRequesterUser(User requesterUser);
 }
