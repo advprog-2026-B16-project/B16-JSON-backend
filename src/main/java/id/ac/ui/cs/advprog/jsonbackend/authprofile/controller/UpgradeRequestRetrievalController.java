c49f215e2fba873e8bd5e3b29122ec237e7b0b15
nzafranf <nzfadhilll@gmail.com>
2026-03-05 18:10:11 +0700

    [REFACTOR] UpgradeRequestRetrievalController.java use DTO and apply Lombok for clean code

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/controller/UpgradeRequestRetrievalController.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/controller/UpgradeRequestRetrievalController.java
@@ -1,28 +1,28 @@
 package id.ac.ui.cs.advprog.jsonbackend.authprofile.controller;
 
+import id.ac.ui.cs.advprog.jsonbackend.authprofile.dto.UpgradeRequestResponse;
 import id.ac.ui.cs.advprog.jsonbackend.authprofile.service.UpgradeRequestRetrievalService;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping("api/upgrade-request")
+@RequestMapping("/api/upgrade-request")
+@RequiredArgsConstructor
 public class UpgradeRequestRetrievalController {
 
-    UpgradeRequestRetrievalService upgradeRequestRetrievalService;
+    private final UpgradeRequestRetrievalService upgradeRequestRetrievalService;
 
-    @Autowired
-    public UpgradeRequestRetrievalController(UpgradeRequestRetrievalService upgradeRequestRetrievalService) {
-        this.upgradeRequestRetrievalService = upgradeRequestRetrievalService;
-    }
-    
-    @GetMapping("/get-requests")
-    public ResponseEntity<?> getUsers() {
-
-        return new ResponseEntity<>(this.upgradeRequestRetrievalService.getAllRequests(), HttpStatus.OK);
+    @GetMapping("/get-all")
+    public ResponseEntity<List<UpgradeRequestResponse>> getAllRequests() {
+        List<UpgradeRequestResponse> responses = upgradeRequestRetrievalService.getAllRequests().stream()
+                .map(UpgradeRequestResponse::fromRequest)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(responses);
     }
 }
