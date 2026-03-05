9a6571113d3ed13c6a24dc5c5774bf02328aacba
nzafranf <nzfadhilll@gmail.com>
2026-03-05 18:09:25 +0700

    [REFACTOR] UserRepository.java use UUID as primary key for robust architecture

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package id.ac.ui.cs.advprog.jsonbackend.authprofile.repository;
 
-import java.util.Optional;
-
+import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import id.ac.ui.cs.advprog.jsonbackend.authprofile.model.User;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 }
