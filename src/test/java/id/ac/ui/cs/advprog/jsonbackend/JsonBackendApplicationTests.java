e71762137e0488b61dbb185cf882e313a4834c8b
erikwilbert <erik.wilbert@ui.ac.id>
2026-03-06 11:37:00 +0700

    Add: test properties

--- src/test/java/id/ac/ui/cs/advprog/jsonbackend/JsonBackendApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/jsonbackend/JsonBackendApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JsonBackendApplicationTests {
 
     @Autowired(required = false)
