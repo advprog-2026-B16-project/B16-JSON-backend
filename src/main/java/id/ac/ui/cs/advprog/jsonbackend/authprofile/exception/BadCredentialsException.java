be1addeb1d4962b0015ae190a64c56038317aaae
nzafranf <nzfadhilll@gmail.com>
2026-03-06 10:05:29 +0700

    [REFACTOR] BadCredentialsException.java create generic exception to prevent user enumeration

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/exception/BadCredentialsException.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.jsonbackend.authprofile.exception;
+
+public class BadCredentialsException extends RuntimeException {
+    public BadCredentialsException(String message) {
+        super(message);
+    }
+}
