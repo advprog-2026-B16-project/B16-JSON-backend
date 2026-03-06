0abd0f0c6afa3d79574763024620111cd4b8e548
erikwilbert <erik.wilbert@ui.ac.id>
2026-03-06 11:46:26 +0700

    Fix: error

--- src/test/java/id/ac/ui/cs/advprog/jsonbackend/JsonBackendApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/jsonbackend/JsonBackendApplicationTests.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class JsonBackendApplicationTests {
 
     @Autowired(required = false)
