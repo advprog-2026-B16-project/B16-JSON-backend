245492e1d3c04bd1c92f1728e62a50d7a6c8551d
nzafranf <nzfadhilll@gmail.com>
2026-03-06 10:05:50 +0700

    [REFACTOR] UserRegistrationRequest.java enforce strong password policy with regex for security

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/dto/UserRegistrationRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/dto/UserRegistrationRequest.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.jsonbackend.authprofile.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +19,10 @@ public class UserRegistrationRequest {
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(
+        regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+        message = "Password must contain at least one digit, one lowercase, one uppercase, one special character, and no whitespace"
+    )
     private String password;
 
     private String confirmPassword;
