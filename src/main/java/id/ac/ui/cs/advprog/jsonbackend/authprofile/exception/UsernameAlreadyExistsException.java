e0bf8fb86d5d2f9142fc307e52a069f65806a2de
nzafranf <nzfadhilll@gmail.com>
2026-03-06 10:05:50 +0700

    [REFACTOR] UsernameAlreadyExistsException.java create specific domain exception for SRP

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/authprofile/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.jsonbackend.authprofile.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
