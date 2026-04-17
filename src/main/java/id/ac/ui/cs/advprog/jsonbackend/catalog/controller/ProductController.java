bcbdbc4fb48b2525155c182199090c8c1d86b6dd
Humam Al Labib <humam.alabib@gmail.com>
2026-04-17 17:44:30 +0700

    feat(catalog): add product controller endpoints

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/catalog/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/catalog/controller/ProductController.java
@@ -1,40 +1,41 @@
 package id.ac.ui.cs.advprog.jsonbackend.catalog.controller;
 
 import id.ac.ui.cs.advprog.jsonbackend.catalog.dto.ProductDTO;
+import id.ac.ui.cs.advprog.jsonbackend.catalog.dto.ProductRequest;
+import id.ac.ui.cs.advprog.jsonbackend.catalog.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "*") // penting untuk FE
+@CrossOrigin(origins = "*")
 public class ProductController {
 
-    @GetMapping
-    public List<ProductDTO> getProducts() {
+    private final ProductService service;
 
-        return List.of(
-                new ProductDTO(
-                        "1",
-                        "KitKat Matcha",
-                        "Japanese Matcha Chocolate",
-                        50000,
-                        10,
-                        "Japan",
-                        LocalDate.of(2025,5,1),
-                        "jastiper1"
-                ),
-                new ProductDTO(
-                        "2",
-                        "Pocky Strawberry",
-                        "Strawberry biscuit sticks",
-                        30000,
-                        20,
-                        "Japan",
-                        LocalDate.of(2025,5,2),
-                        "jastiper2"
-                )
-        );
+    public ProductController(ProductService service) {
+        this.service = service;
     }
+
+    @GetMapping
+    public List<ProductDTO> getAll() {
+        return service.findAllProducts();
+    }
+
+    @PostMapping
+    public ProductDTO create(@RequestBody ProductRequest request) {
+        return service.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public ProductDTO update(@PathVariable String id, @RequestBody ProductRequest request) {
+        return service.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
+
 }
