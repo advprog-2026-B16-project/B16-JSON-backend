ba7932668dc7f90db539251dd935bbd2688c4b99
Humam Al Labib <humam.alabib@gmail.com>
2026-04-17 17:16:42 +0700

    Add Dummy ProductController

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/catalog/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/catalog/controller/ProductController.java
@@ -1,33 +1,40 @@
 package id.ac.ui.cs.advprog.jsonbackend.catalog.controller;
 
 import id.ac.ui.cs.advprog.jsonbackend.catalog.dto.ProductDTO;
-import id.ac.ui.cs.advprog.jsonbackend.catalog.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "*") // penting untuk FE
 public class ProductController {
 
-    private final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
-
     @GetMapping
-    public List<ProductDTO> getAllProducts() {
-        return productService.findAllProducts();
-    }
+    public List<ProductDTO> getProducts() {
 
-    @GetMapping("/search")
-    public List<ProductDTO> searchProducts(@RequestParam String keyword) {
-        return productService.searchByName(keyword);
-    }
-
-    @GetMapping("/jastiper/{jastiperId}")
-    public List<ProductDTO> getByJastiper(@PathVariable String jastiperId) {
-        return productService.findByJastiper(jastiperId);
+        return List.of(
+                new ProductDTO(
+                        "1",
+                        "KitKat Matcha",
+                        "Japanese Matcha Chocolate",
+                        50000,
+                        10,
+                        "Japan",
+                        LocalDate.of(2025,5,1),
+                        "jastiper1"
+                ),
+                new ProductDTO(
+                        "2",
+                        "Pocky Strawberry",
+                        "Strawberry biscuit sticks",
+                        30000,
+                        20,
+                        "Japan",
+                        LocalDate.of(2025,5,2),
+                        "jastiper2"
+                )
+        );
     }
 }
